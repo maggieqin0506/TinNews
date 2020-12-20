6a4ee44a137230201ea72ef2f9e479236df414d2
maggieqin0506 <yucong.qin@vanderbilt.edu>
2020-12-19 23:54:21 -0600

    basic navigation done

---
+++ app/src/main/java/com/laioffer/tinnews/TinNewsApplication.java
@@ -0,0 +1,19 @@
+package com.laioffer.tinnews;
+
+import android.app.Application;
+
+import com.ashokvarma.gander.Gander;
+import com.ashokvarma.gander.imdb.GanderIMDB;
+import com.facebook.stetho.Stetho;
+
+// app level configuration so as to have getter library connect to the app
+public class TinNewsApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Gander.setGanderStorage(GanderIMDB.getInstance());
+        Stetho.initializeWithDefaults(this);
+
+    }
+}
---
+++ app/src/main/java/com/laioffer/tinnews/ui/home/HomeViewModel.java
@@ -0,0 +1,38 @@
+package com.laioffer.tinnews.ui.home;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.Transformations;
+import androidx.lifecycle.ViewModel;
+
+import com.laioffer.tinnews.repository.NewsRepository;
+
+import model.NewsResponse;
+
+// GOOGLE MVVM provides
+public class HomeViewModel extends ViewModel {
+
+    private final NewsRepository repository;
+    // hold the data
+    private final MutableLiveData<String> countryInput = new MutableLiveData<>();
+
+    public HomeViewModel(NewsRepository newsRepository) {
+        this.repository = newsRepository;
+    }
+
+    public void setCountryInput(String country) {
+        countryInput.setValue(country);
+    }
+
+    public LiveData<NewsResponse> getTopHeadlines() {
+        // give the input data to getTopHeadlines method
+        // repository:: method reference
+        // we need to save the state
+
+        // 生产线建好了，只有一条生产线
+        // 只需要做一次，新建live data object
+        // observe pattern: automatically create an output
+        return Transformations.switchMap(countryInput, repository::getTopHeadlines);
+    }
+}
+
